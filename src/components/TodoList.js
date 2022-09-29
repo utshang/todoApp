ea0aede706a90d60b275544909336ba5480e3eed
Ashley Shang <utshang216@gmail.com>
2022-09-29 14:31:51 +0800

    add getTodo and filterTodo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from "./Context";
 import "../stylesheets/_todolist.scss";
 import { Link, useNavigate } from "react-router-dom";
 import AddBtn from "./AddBtn";
 import { logOut } from "../services/callAPI";
+import { getTodo } from "../services/callAPI";
 
 function TodoList() {
   let navigate = useNavigate();
   // const { user, token } = useAuth();
   const { user, setUser, token, setToken } = useAuth();
-
   const [todoState, setTodoState] = useState("all");
+  const [data, setData] = useState([]);
+  const [text, setText] = useState("");
+
   const [stateList] = useState([
     {
       todoState: "all",
@@ -26,6 +29,23 @@ function TodoList() {
     },
   ]);
 
+  //取得 todo
+  const getTodoList = async (e) => {
+    await getTodo(token)
+      .then((response) => {
+        console.log(response);
+        setData(response.data?.todos);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getTodoList();
+  }, []);
+
+  //登出
   const logout = async (e) => {
     e.preventDefault();
     await logOut(token)
@@ -40,11 +60,22 @@ function TodoList() {
       });
   };
 
+  //tab 切換
   const changeState = (e, item) => {
     e.preventDefault();
     setTodoState(item.todoState);
     console.log(item);
   };
+
+  //篩選 todo
+  const filterTodo = () => {
+    if (todoState === "completed")
+      return data.filter((item) => item.completed_at);
+    else if (todoState === "active")
+      return data.filter((item) => !item.completed_at);
+    else return data;
+  };
+
   // fetch 寫法
   // const logout = () => {
   //   const _url = "https://todoo.5xcamp.us/users/sign_out";
@@ -71,7 +102,7 @@ function TodoList() {
         <h1 className="fw-bold fs-1">TODO</h1>
         <div>
           <a
-            href="/"
+            href="#"
             className="mb-2 d-flex justify-content-end"
             onClick={logout}
           >
@@ -82,7 +113,7 @@ function TodoList() {
           </p>
         </div>
       </div>
-      <AddBtn />
+      <AddBtn text={text} setText={setText} getTodoList={getTodoList} />
       <div className="bg-white rounded shadow ">
         <div>
           <ul className="d-flex justify-content-between pt-2 mb-2 list-tab">
@@ -103,20 +134,28 @@ function TodoList() {
         </div>
 
         <ul>
-          <li className="border-1 border-bottom p-4 d-flex justify-content-between">
-            <div className="d-flex align-items-start">
-              <label htmlFor="add-btn"></label>
-              <input type="checkbox" name="add-btn" className="me-3" />
-              <span>煮飯</span>
-            </div>
-            <a href="#">
-              <i className="bi bi-trash text-muted"></i>
-            </a>
-          </li>
+          {data.map((item, i) => {
+            return (
+              <li
+                key={i}
+                className="border-1 border-bottom p-4 d-flex justify-content-between"
+              >
+                <div className="d-flex align-items-start">
+                  <label htmlFor="add-btn"></label>
+                  <input type="checkbox" name="add-btn" className="me-3" />
+                  <span>{item.content}</span>
+                </div>
+                <a href="#">
+                  <i className="bi bi-trash text-muted"></i>
+                </a>
+              </li>
+            );
+          })}
         </ul>
         <div className="p-4 d-flex justify-content-between align-items-end list-footer fs-7 text-muted">
           <span>
-            <span>5</span> items left
+            <span>{data.filter((item) => !item.completed_at).length} </span>
+            items left
           </span>
           <Link to="/">
             <span>Clear Completed</span>
